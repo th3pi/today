eadbdfab49accdda14bf3a3c85544378642ff9a2
th3pi <th3pizzaman15@gmail.com>
2020-06-25 15:33:05 -0400

    removed createError

--- src/app.js
+++ src/app.js
@@ -27,15 +27,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    next(createError(404));
-});
-app.use((err, req, res) => {
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-    res.status(err.status || 500);
-    res.render('error');
-})
 
 app.use('/tasks', taskRoutes);
 
